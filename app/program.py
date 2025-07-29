e2794020c03fb9c24b0de974018acb5552560291
GavinHungaski <ghungaski@gmail.com>
2025-07-29 16:50:13 -0400

    file rework, ui file is only ui and scraper logic now in program.py

--- app/program.py
+++ app/program.py
@@ -1,5 +1,13 @@
 from ui import *
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from datetime import datetime
+from bs4 import BeautifulSoup
 import logging
+import requests
+import logging
+import time
 
 
 def initialize():
@@ -17,8 +25,153 @@ def initialize():
 def main():
     initialize()
     root = tk.Tk()
-    _ = ScraperUI(master=root)
+    _ = ScraperUI(master=root, scraper_function=scrape_and_send)
     root.mainloop()
+
+
+def scrape_and_send():
+        while True:
+            try:
+                seen_listings = get_seen_listings()
+                ScraperUI.write_to_info("Getting links . . .\n")
+                links = ScraperUI.get_links()
+                for link in links:
+                    ScraperUI.write_to_info(f"Now scraping: \n{link}")
+                    cars = scrape_craigslist(link)
+                    ScraperUI.write_to_info(
+                        f"Finished scraping @ {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
+                    )
+                    for car in cars:
+                        listing_id = car['link'].split('/')[-1]
+                        if listing_id not in seen_listings:
+                            ScraperUI.write_to_info(
+                                f"\nNew listing found: {listing_id}, sending to discord.")
+                            message = construct_payload(car)
+                            send_discord_message(message)
+                            add_seen_listing(listing_id)
+                ScraperUI.write_to_info(
+                    f"\nNow waiting {ScraperUI.sleep_time} minutes until next scrape . . .")
+                time.sleep(ScraperUI.sleep_time * 60)
+            except Exception as e:
+                logging.error(f"An error occurred: {e}")
+                return
+
+def get_discord_login(file_path='./discord.txt'):
+    try:
+        with open(file_path, 'r') as file:
+            channel_url = file.readline()
+            auth = file.readline()
+        return channel_url, auth
+    except Exception as e:
+        ScraperUI.write_to_info(f"\nError: {e}")
+        return
+
+
+def get_seen_listings(file_path='./data/seen_listings.txt'):
+    try:
+        with open(file_path, 'r') as file:
+            seen_listings = file.read().splitlines()
+        return seen_listings
+    except FileNotFoundError:
+        return []
+
+def add_seen_listing(listing_id):
+    with open('./data/seen_listings.txt', 'a') as file:
+        file.write(f"{listing_id}\n")
+
+def send_discord_message(message):
+    ScraperUI.write_to_info(message)
+    channel_url, auth = get_discord_login()
+    payload = {"content": message}
+    headers = {"Authorization": auth}
+    response = requests.post(
+        channel_url, json=payload, headers=headers)
+    if response.status_code != 200:
+        logging.error(f"Failed to send Discord message: {response.text}")
+
+def construct_payload(car):
+    try:
+        message = (
+            f"Title: {car.get('title', 'Not Available')}\n"
+            f"Odometer: {car.get('odometer', 'Not Available')}\n"
+            f"Date: {car.get('date', 'Not Available')}\n"
+            f"Price: {car.get('price', 'Not Available')}\n"
+            f"Link: {car.get('link', 'Not Available')}"
+        )
+    except Exception as e:
+        ScraperUI.write_to_info(f"Error: {e}")
+        message = None
+    return message
+
+def scrape_craigslist(link):
+    try:
+        content = get_html_selenium(link)
+        soup = BeautifulSoup(content, 'html.parser')
+        cars = get_cars(soup)
+        return cars
+    except requests.exceptions.RequestException as e:
+        ScraperUI.write_to_info(f"Error during web request: {e}")
+        return None
+    except Exception as e:
+        ScraperUI.write_to_info(f"An error occurred: {e}")
+        return None
+
+
+def get_html_selenium(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
+    driver.get(url)
+    time.sleep(2)
+    html = driver.page_source
+    driver.quit()
+    return html
+
+
+def get_cars(soup):
+    cars = []
+    for div in soup.find_all('div', {"class": "gallery-card"}):
+        car = {}
+
+        link = div.find('a').get('href').strip()
+        car['link'] = link
+
+        title = div.find('span', class_='label')
+        if title:
+            title = title.text.strip()
+            car['title'] = title
+
+        price = div.find('span', class_='priceinfo')
+        if price:
+            price = price.text.strip()
+            car['price'] = price
+
+        info = div.find('div', class_='meta')
+        if info:
+            info = info.text.strip()
+            date = get_date(info)
+            car['date'] = date
+            odometer = get_odometer(date, info)
+            car['odometer'] = f"{odometer} mi"
+
+        cars.append(car)
+    return cars
+
+
+def get_date(info):
+    date_str = info[:5]
+    try:
+        date = datetime.strptime(date_str, '%m/%d')
+        return date.strftime('%m/%d')
+    except ValueError:
+        date = info[:4]
+        return date
+
+
+def get_odometer(date, info):
+    info = info.replace(date, "").split()
+    return info[0]
 
 
 if __name__ == "__main__":
--- app/ui.py
+++ app/ui.py
@@ -1,18 +1,10 @@
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from tkinter import scrolledtext
-from selenium import webdriver
-from datetime import datetime
-from bs4 import BeautifulSoup
 import tkinter as tk
 import threading
-import requests
-import logging
-import time
 
 
 class ScraperUI:
-    def __init__(self, master=None):
+    def __init__(self, master=None, scraper_function=None):
         self.sleep_time = 10
         self.scraper_running = False
         self.master = master
@@ -76,54 +68,17 @@ class ScraperUI:
         if not self.scraper_running:
             if len(self.get_links()) > 0:
                 self.write_to_info("Starting up . . .\n")
+                self.scraper_running = True
                 self.scrape_thread = threading.Thread(
-                    target=self.scrape_and_send)
+                    target=self.scraper_function, args=(self,))
                 self.scrape_thread.daemon = True
                 self.scrape_thread.start()
-                self.scraper_running = True
             else:
                 self.write_to_info(
                     "No links found, add links and retry")
         else:
             self.write_to_info(
                 "Already running . . .")
-
-    def scrape_and_send(self):
-        while True:
-            try:
-                seen_listings = self.get_seen_listings()
-                self.write_to_info("Getting links . . .\n")
-                links = self.get_links()
-                for link in links:
-                    self.write_to_info(f"Now scraping: \n{link}")
-                    cars = self.scrape_craigslist(link)
-                    self.write_to_info(
-                        f"Finished scraping @ {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n"
-                    )
-                    for car in cars:
-                        listing_id = car['link'].split('/')[-1]
-                        if listing_id not in seen_listings:
-                            self.write_to_info(
-                                f"\nNew listing found: {listing_id}, sending to discord.")
-                            message = self.construct_payload(car)
-                            self.send_discord_message(message)
-                            self.add_seen_listing(listing_id)
-                self.write_to_info(
-                    f"\nNow waiting {self.sleep_time} minutes until next scrape . . .")
-                time.sleep(self.sleep_time * 60)
-            except Exception as e:
-                logging.error(f"An error occurred: {e}")
-                return
-
-    def get_discord_login(self, file_path='./discord.txt'):
-        try:
-            with open(file_path, 'r') as file:
-                channel_url = file.readline()
-                auth = file.readline()
-            return channel_url, auth
-        except Exception as e:
-            self.write_to_info(f"\nError: {e}")
-            return
 
     def get_links(self, file_path='./links.txt'):
         try:
@@ -134,109 +89,3 @@ class ScraperUI:
         except FileNotFoundError:
             self.write_to_info(f"File {file_path} not found.")
             return []
-
-    def get_seen_listings(self, file_path='./data/seen_listings.txt'):
-        try:
-            with open(file_path, 'r') as file:
-                seen_listings = file.read().splitlines()
-            return seen_listings
-        except FileNotFoundError:
-            return []
-
-    def add_seen_listing(self, listing_id):
-        with open('./data/seen_listings.txt', 'a') as file:
-            file.write(f"{listing_id}\n")
-
-    def send_discord_message(self, message):
-        self.write_to_info(message)
-        channel_url, auth = self.get_discord_login()
-        payload = {"content": message}
-        headers = {"Authorization": auth}
-        response = requests.post(
-            channel_url, json=payload, headers=headers)
-        if response.status_code != 200:
-            logging.error(f"Failed to send Discord message: {response.text}")
-
-    def construct_payload(self, car):
-        try:
-            message = (
-                f"Title: {car.get('title', 'Not Available')}\n"
-                f"Odometer: {car.get('odometer', 'Not Available')}\n"
-                f"Date: {car.get('date', 'Not Available')}\n"
-                f"Price: {car.get('price', 'Not Available')}\n"
-                f"Link: {car.get('link', 'Not Available')}"
-            )
-        except Exception as e:
-            self.write_to_info(f"Error: {e}")
-            message = None
-        return message
-
-    def scrape_craigslist(self, link):
-        try:
-            content = get_html_selenium(link)
-            soup = BeautifulSoup(content, 'html.parser')
-            cars = get_cars(soup)
-            return cars
-        except requests.exceptions.RequestException as e:
-            self.write_to_info(f"Error during web request: {e}")
-            return None
-        except Exception as e:
-            self.write_to_info(f"An error occurred: {e}")
-            return None
-
-
-def get_html_selenium(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-    driver = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=options)
-    driver.get(url)
-    time.sleep(2)
-    html = driver.page_source
-    driver.quit()
-    return html
-
-
-def get_cars(soup):
-    cars = []
-    for div in soup.find_all('div', {"class": "gallery-card"}):
-        car = {}
-
-        link = div.find('a').get('href').strip()
-        car['link'] = link
-
-        title = div.find('span', class_='label')
-        if title:
-            title = title.text.strip()
-            car['title'] = title
-
-        price = div.find('span', class_='priceinfo')
-        if price:
-            price = price.text.strip()
-            car['price'] = price
-
-        info = div.find('div', class_='meta')
-        if info:
-            info = info.text.strip()
-            date = get_date(info)
-            car['date'] = date
-            odometer = get_odometer(date, info)
-            car['odometer'] = f"{odometer} mi"
-
-        cars.append(car)
-    return cars
-
-
-def get_date(info):
-    date_str = info[:5]
-    try:
-        date = datetime.strptime(date_str, '%m/%d')
-        return date.strftime('%m/%d')
-    except ValueError:
-        date = info[:4]
-        return date
-
-
-def get_odometer(date, info):
-    info = info.replace(date, "").split()
-    return info[0]
