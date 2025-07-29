eae2555ff744b3b99b01ac80d351577023582a75
GavinHungaski <ghungaski@gmail.com>
2025-07-29 17:00:03 -0400

    small name change in imports

--- app/program.py
+++ app/program.py
@@ -1,4 +1,4 @@
-from ui import *
+from ui import ScraperUI
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
